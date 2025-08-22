453538e6dee103b7bc3c0e5ca0d7e4d71345d470
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-08-22 11:54:12 +0100

    change contents doesn't change contents huh?

--- src/main/msp/msp.c
+++ src/main/msp/msp.c
@@ -753,9 +753,11 @@ static bool mspCommonProcessOutCommand(int16_t cmdMSP, sbuf_t *dst, mspPostProce
     }
 
     case MSP_BUILD_INFO:
-        buildDate+=1;
+//        buildDate+=1;
+        buildDate[2]=97;
         bprintf("want to write build date %s from %p of length %d to %p", buildDate, buildDate, BUILD_DATE_LENGTH, dst->ptr);
-        buildDate-=1;
+        bprintf("buildDate[2] = %d", buildDate[2]);
+//        buildDate-=1;
         bprintf("SCB CCR now set to %08x", SCB->CCR);
         sbufWriteData(dst, buildDate, BUILD_DATE_LENGTH);
         bprintf("did that, next btl from %p to %p", buildTime, dst->ptr);
