6d5c7839bd27fb1570386517ed15d7c8292c262e
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-09-04 15:46:26 +0100

    pragma once for pico_mct8329a.h

--- src/platform/PICO/pico_mct8329a.h
+++ src/platform/PICO/pico_mct8329a.h
@@ -19,6 +19,8 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 typedef struct mctLookup_s { uint8_t reg; const char *name; } mctLookup_t;
 extern mctLookup_t mctLookup[];
 extern const int numMCTregs;
