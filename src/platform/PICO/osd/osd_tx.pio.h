36a068d8abd67a3210c0bfe67a0db15d3ab21aef
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-10-03 22:22:49 +0100

    dithered 50% grey

--- src/platform/PICO/osd/osd_tx.pio.h
+++ src/platform/PICO/osd/osd_tx.pio.h
@@ -13,7 +13,7 @@
 // ------ //
 
 #define osd_tx_wrap_target 0
-#define osd_tx_wrap 29
+#define osd_tx_wrap 30
 #define osd_tx_pio_version 0
 
 static const uint16_t osd_tx_program_instructions[] = {
@@ -44,17 +44,18 @@ static const uint16_t osd_tx_program_instructions[] = {
     0x00d9, // 23: jmp    pin, 25
     0x0000, // 24: jmp    0
     0xa046, // 25: mov    y, isr
-    0xe002, // 26: set    pins, 2
-    0x089a, // 27: jmp    y--, 26                [8]
-    0xe000, // 28: set    pins, 0
-    0x0054, // 29: jmp    x--, 20
+    0xe402, // 26: set    pins, 2                [4]
+    0xe003, // 27: set    pins, 3
+    0x029a, // 28: jmp    y--, 26                [2]
+    0xe000, // 29: set    pins, 0
+    0x0054, // 30: jmp    x--, 20
             //     .wrap
 };
 
 #if !PICO_NO_HARDWARE
 static const struct pio_program osd_tx_program = {
     .instructions = osd_tx_program_instructions,
-    .length = 30,
+    .length = 31,
     .origin = -1,
     .pio_version = osd_tx_pio_version,
 #if PICO_PIO_VERSION > 0
