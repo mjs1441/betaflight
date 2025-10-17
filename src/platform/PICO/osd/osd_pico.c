25f62efd4fc0ca7c6af6af1e3c76431a29fd97cf
Matthew Selby <matthew_selby@yahoo.co.uk>
2025-10-17 16:10:43 +0100

    tidy a bit

--- src/platform/PICO/osd/osd_pico.c
+++ src/platform/PICO/osd/osd_pico.c
@@ -139,12 +139,7 @@ int64_t safe_zone_callback(alarm_id_t id, void * user_data)
 
 bool osdBuffer1Safe(void)
 {
-    // TODO *** add back in dma check
-    return in_safe_zone;
-//    return in_safe_zone && !dma_channel_is_busy(dma_chan_buf1_to_buf2) && !dma_channel_is_busy(dma_chan_zero_to_buf1);
-//    return // !dma_channel_is_busy(dma_chan_buf1_to_buf2) &&
-//        !dma_channel_is_busy(dma_chan_zero_to_buf1);
-//    return  !dma_channel_is_busy(dma_chan_buf1_to_buf2);
+    return in_safe_zone && !dma_channel_is_busy(dma_chan_buf1_to_buf2) && !dma_channel_is_busy(dma_chan_zero_to_buf1);
 }
 
 void testUpdate(void);
@@ -197,11 +192,7 @@ static void plotBorder(void)
 void osd_test_init(void)
 {
     safe_zone_period = 18000;
-    safe_zone_period = 10000; // half of PAL 20000us, disallow TRANSFER (render to osdBuffer1) during final 10000 or so
-//    safe_zone_period = 16723; // half of PAL 20000us, disallow TRANSFER (render to osdBuffer1) during final 10000 or so
-//    safe_zone_period = 16000; // half of PAL 20000us, disallow TRANSFER (render to osdBuffer1) during final 10000 or so
-//    safe_zone_period = 1234; // half of PAL 20000us, disallow TRANSFER (render to osdBuffer1) during final 10000 or so
-//    safe_zone_period = 220100;
+//    safe_zone_period = 10000; // half of PAL 20000us, disallow TRANSFER (render to osdBuffer1) during final 10000 or so
     in_safe_zone = true;
 
     bprintf("osd_test_init");
@@ -562,6 +553,7 @@ static void vsync_callback(void)
     
     if (c % 250 == 0) {
         bprintf("%d vsync_callback busy %d %d",c, business, busybuf);
+        // NB ave wraps quickly (~1000 vsyncs)
         bprintf("max time between callbacks: %d, last: %d, ave: %.1f",vmax/150, q/150, (double)(((float)qtot)/c/150));
         vmax = 0;
 #ifdef unsafetestloop
@@ -782,21 +774,15 @@ void testOSDtaskOffPidLoop(void)
 
 void osdUpdateCallback(uint32_t t_us)
 {
-#if 1
+#if 0
     ouccount++;
     if (!osdBuffer1Safe()) {
         oucunsafe++;
         testUpdate();
     } else {
-        testUpdate();//        oucunsafe++;
+        // put equivalent delay here if you want to measure proportions from counters
     }
     UNUSED(t_us);
-    
-#elif 1
-    UNUSED(t_us);
-    ouccount++;
-    oucunsafe += !osdBuffer1Safe();
-    testUpdate(); // delayMicroseconds(755);
 #elif defined unsafetestloop
     UNUSED(t_us);
     while (true) {
@@ -810,21 +796,13 @@ void osdUpdateCallback(uint32_t t_us)
     static char oucbuf[30];
     ouccount++;
     sze = getCycleCounter();
-//    if (vsyncflag) {
-//        vsyncflag=0;
-//        delayMicroseconds(vdelay);
-//        vdelay = 18000 + (vdelay+1) % 2000;
-//    }
-//    osdPrintFloat(oucbuf, 0x64, ((float)t_us)/10000, "", 3, false, 0x6c);
-//    osdPioWrite(2,0,"fish"); UNUSED(oucbuf); UNUSED(t_us);
-//    osdPioWrite(2,0,oucbuf);
+    osdPrintFloat(oucbuf, 0x64, ((float)t_us)/10000, "", 3, false, 0x6c);
+    osdPioWrite(2,0,oucbuf);
     if (osdBuffer1Safe()) {
         testUpdate();
     } else {
         oucunsafe++;
     }
-    //  osdPioWrite(2,0,"fish");
-    UNUSED(oucbuf); UNUSED(t_us);
 #endif
 }
 
