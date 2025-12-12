dacf5db9bae3d9262304423756dadbba4a5cb41a
Matthew Selby <matthew.selby@raspberrypi.com>
2025-12-12 14:05:27 +0000

    reorder target.h a bit

--- src/platform/PICO/target/RP2350B/target.h
+++ src/platform/PICO/target/RP2350B/target.h
@@ -93,6 +93,9 @@
 #define DMA_IRQ_CORE_NUM 1 // Use core 1 for DMA IRQs
 #undef USE_DMA_SPEC // not yet required - possibly won't be used at all
 
+#undef USE_DSHOT_BITBANG
+#define USE_DSHOT_TELEMETRY
+
 // Radio RX
 // SERIALRX CRSF supported, also TELEMETRY_CRSF
 // #undef USE_CRSF
@@ -159,7 +162,6 @@
 #undef USE_MSP_DISPLAYPORT
 
 #undef USE_DSHOT_BITBANG
-#define USE_DSHOT_TELEMETRY
 #undef USE_ESC_SENSOR
 
 #undef USE_VTX
