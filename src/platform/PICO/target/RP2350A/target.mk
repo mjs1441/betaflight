afe5f39e7ae82e1ec0bb14b0fb6fb862778d773d
Matthew Selby <matthew.selby@raspberrypi.com>
2026-01-15 19:05:20 +0000

    PICO: allow flash settings from board-specific config.mk

--- src/platform/PICO/target/RP2350A/target.mk
+++ src/platform/PICO/target/RP2350A/target.mk
@@ -1,13 +1,23 @@
 TARGET_MCU        := RP2350A
 TARGET_MCU_FAMILY := RP2350
 
+
+# MCU_FLASH_SIZE may be defined in board-specific config.mk
+ifeq ($(MCU_FLASH_SIZE),)
+# default to 4MB
 MCU_FLASH_SIZE  = 4096
+endif
 
 DEVICE_FLAGS    += -DPICO_RP2350A=1
 
 # For pico-sdk, define flash-related attributes
-# 4194304 = 4 * 1024 * 1024
-DEVICE_FLAGS    += \
+# TARGET_DEVICE_FLAGS may be defined in board-specific config.mk
+ifeq ($(TARGET_DEVICE_FLAGS),)
+# default to 4194304 = 4 * 1024 * 1024 on W25Q080
+TARGET_DEVICE_FLAGS  = \
                    -DPICO_FLASH_SPI_CLKDIV=2 \
                    -DPICO_FLASH_SIZE_BYTES=4194304 \
                    -DPICO_BOOT_STAGE2_CHOOSE_W25Q080=1
+endif
+
+DEVICE_FLAGS += $(TARGET_DEVICE_FLAGS)
--- src/platform/PICO/target/RP2350B/target.mk
+++ src/platform/PICO/target/RP2350B/target.mk
@@ -1,14 +1,23 @@
 TARGET_MCU        := RP2350B
 TARGET_MCU_FAMILY := RP2350
 
+# MCU_FLASH_SIZE may be defined in board-specific config.mk
+ifeq ($(MCU_FLASH_SIZE),)
+# default to 8MB
 MCU_FLASH_SIZE  = 8192
+endif
 
 # In pico-sdk, PICO_RP2350A=0 means RP2350B family.
 DEVICE_FLAGS    += -DPICO_RP2350A=0
 
 # For pico-sdk, define flash-related attributes
-# 8388608 = 8 * 1024 * 1024
-DEVICE_FLAGS    += \
+# TARGET_DEVICE_FLAGS may be defined in board-specific config.mk
+ifeq ($(TARGET_DEVICE_FLAGS),)
+# default to 8388608 = 8 * 1024 * 1024 on W25Q080
+TARGET_DEVICE_FLAGS  = \
                    -DPICO_FLASH_SPI_CLKDIV=2 \
                    -DPICO_FLASH_SIZE_BYTES=8388608 \
                    -DPICO_BOOT_STAGE2_CHOOSE_W25Q080=1
+endif
+
+DEVICE_FLAGS += $(TARGET_DEVICE_FLAGS)
